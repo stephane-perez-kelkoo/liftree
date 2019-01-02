9afc9fee79d871976cb0e693e23dcb760d78bc82
stephane.perez <stephane.perez@kelkoogroup.com>
2019-01-02 17:16:10 +0100

    Use Apache httpd with uWSGI

--- docker_image_build_and_run.sh
+++ docker_image_build_and_run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+docker rm liftree --force
 docker build -t perezste/liftree -f docker/Dockerfile .
 docker run -d -p 8080:80 --name liftree perezste/liftree:latest
 docker exec -it liftree /bin/bash
